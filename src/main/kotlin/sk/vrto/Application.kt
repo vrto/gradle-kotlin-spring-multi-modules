27cf10b905cb3a66901641c685c671bd955c2424
vrto <michal.vrtiak+github@gmail.com>
2018-11-22 15:18:29 +0100

    Cleanup

--- src/main/kotlin/sk/vrto/Application.kt
+++ src/main/kotlin/sk/vrto/Application.kt
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class Application {
-
-}
+class Application
 
 fun main(args: Array<String>) {
     SpringApplication.run(Application::class.java, *args)
